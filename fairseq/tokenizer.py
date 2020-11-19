3b709280b1a8466410943c85362e05aa995a3ea9
Peter St. John <pstjohn@login5.summit.olcf.ornl.gov>
2020-11-19 17:39:17 -0500

    adding character-level change

--- fairseq/tokenizer.py
+++ fairseq/tokenizer.py
@@ -9,7 +9,11 @@ import re
 SPACE_NORMALIZER = re.compile(r"\s+")
 
 
+# def tokenize_line(line):
+#     line = SPACE_NORMALIZER.sub(" ", line)
+#     line = line.strip()
+#     return line.split()
+
 def tokenize_line(line):
-    line = SPACE_NORMALIZER.sub(" ", line)
-    line = line.strip()
-    return line.split()
+    """For character-level model"""
+    return list(line.strip())
