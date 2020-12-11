03ecb8e5b3b627f86ccfe0935a9aa34e1f564570
Peter St. John <peterc.stjohn@gmail.com>
2020-12-11 09:30:43 -0700

    undoing char level tokenizer

--- fairseq/tokenizer.py
+++ fairseq/tokenizer.py
@@ -9,11 +9,11 @@ import re
 SPACE_NORMALIZER = re.compile(r"\s+")
 
 
-# def tokenize_line(line):
-#     line = SPACE_NORMALIZER.sub(" ", line)
-#     line = line.strip()
-#     return line.split()
-
 def tokenize_line(line):
-    """For character-level model"""
-    return list(line.strip())
+    line = SPACE_NORMALIZER.sub(" ", line)
+    line = line.strip()
+    return line.split()
+
+# def tokenize_line(line):
+#     """For character-level model"""
+#     return list(line.strip())
